36aafea612fa02177751c45331e9f55608cfd690
James Daniel <james.j.daniel@outlook.com>
2016-09-02 06:32:41 +0000

    ENH7: coreos - Automate deployment of network-config for each node #8

--- cloud-config/rocket.sh
+++ cloud-config/rocket.sh
@@ -4,3 +4,8 @@ scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-co
 scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/cloud-config/cloud-config_n1.yml core@10.5.5.13:~/
 scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/cloud-config/cloud-config_n2.yml core@10.5.5.14:~/
 scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/cloud-config/cloud-config_n3.yml core@10.5.5.15:~/
+
+scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/network-config/network-config_n0.json core@10.5.5.12:~/
+scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/network-config/network-config_n1.json core@10.5.5.13:~/
+scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/network-config/network-config_n2.json core@10.5.5.14:~/
+scp -i /home/jdaniel/.ssh/id_rsa.pub /home/jdaniel/git-securitydaemons/sdmfle-conf/coreos/network-config/network-config_n3.json core@10.5.5.15:~/
